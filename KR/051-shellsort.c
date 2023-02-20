54567cf53b853d10233a63c1e5d901d9afd874fc
HuiLiang <CrazyHuiLiang@163.com>
2023-02-20 21:16:29 +0800

    review shell sort

--- KR/051-shellsort.c
+++ KR/051-shellsort.c
@@ -4,14 +4,19 @@
 void shellsort(int v[], int n) {
   int gap, i, j, temp;
 
-  for (gap = n/2; gap > 0; gap /=2)
-    for (i = gap; i< n; i++)
-      for (j=i-gap; j>=0 && v[j]>v[j+gap]; j-=gap) {
-        temp = v[j];
-        v[j] = v[j+gap];
-        v[j+gap] = temp;
+  for (gap = n/2; gap > 0; gap /=2) { // 控制两个比较元素之间的距离
+    for (i = gap; i< n; i++) {        // 用于元素间移动位置，从第一个gap作为下标的位置，向后遍历至数组结尾
+      for (j=i-gap; j>=0; j-=gap) {   // 依次向前比较各对相距gap个位置的元素
+        if (v[j]>v[j+gap]) {          // 逆序则交换
+          temp = v[j];
+          v[j] = v[j+gap];
+          v[j+gap] = temp;
+        }
       }
+    }
+  }
 }
+
 int main(){
   
   int v[] = {1, 3, 9, 2, 11, 18, 19, 5};
