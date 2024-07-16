970dd83541291b30a967360d96ce764ff227f406
huiliang.zhang <huiliang.zhang@jinshuschool.com>
2024-07-16 12:19:28 +0800

    微调文档

--- KR/002-hello.c
+++ KR/002-hello.c
@@ -1,7 +1,11 @@
 // 梦开始的地方：打印Hello world
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 	printf("hello world \n\n");
+    char c1[] = {'1', '2', '3', '4', '5', '\0'};
+    char c2[] = "12345";
+    printf("c1 length: %zu, c2 length: %zu\n", strlen(c1), strlen(c2));
 	return 0;
 }
